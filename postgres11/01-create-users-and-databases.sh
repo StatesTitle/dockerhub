f1186e13f0de59f6d9330d57bf2c78ea8a06a8cb
Charlie Saunders <charlieasaunders@gmail.com>
2019-12-03 09:05:44 -0500

    Use a different environment variable for Postgres11 database names.

--- postgres11/01-create-users-and-databases.sh
+++ postgres11/01-create-users-and-databases.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 /wait-for-postgres
-for db in ${ST_DATABASE_NAMES}; do
+for db in ${POSTGRES11_DATABASE_NAMES}; do
 	createuser -U postgres -s ${db}
 	createdb -U postgres ${db} --owner=${db}
 done
--- postgres11/10-possibly-load-backup.sh
+++ postgres11/10-possibly-load-backup.sh
@@ -4,7 +4,7 @@ if ls /dumps/*.dump 1> /dev/null 2>&1; then
     /wait-for-postgres
     for f in /dumps/*.dump ; do
         service_name=$(echo ${f} | sed -e 's/\/dumps\/latest-st-// ; s/.dump// ; s/--staging// ; s/-/_/')
-        if [[ "$ST_DATABASE_NAMES" == *"$service_name"* ]] ; then
+        if [[ "$POSTGRES11_DATABASE_NAMES" == *"$service_name"* ]] ; then
             # Ignore errors in the restore as it attempts to drop tables first
             pg_restore --verbose --clean --no-acl --no-owner -U postgres -d "$service_name" "$f" || true
         fi
